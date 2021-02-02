3b4a70d6bd49d1db084b24d30ff8a70730fe0738
ganesh <ganesh@LAPTOP-RIP0I1CO>
2021-02-02 13:36:02 +0530

    First commit...

---
+++ testmodule.py
@@ -0,0 +1,17 @@
+'''
+Created on 18-Jan-2021
+
+@author: ganesh
+'''
+
+
+mylist = ["apple", "banana", "cherry", 3]
+print(mylist)
+print("List length:", len(mylist))
+print(type(mylist))
+mylist.append(5)
+print(mylist)
+mylist.remove(3)
+print(mylist)
+mylist.insert(0,31)
+print(mylist)
